a096bef9e638ef887962aa6b1857917032b4387e
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-01-07 12:15:00 +0100

    logo ok

--- Client/src/components/Header/Logo.jsx
+++ Client/src/components/Header/Logo.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
+/**
+ * 🎨 LOGO COMPONENT - TEKACOM
+ * Taille optimale pour header moderne (capture 2)
+ */
+
 const Logo = ({ scrolled }) => {
   return (
     <Link
       to="/"
-      className="relative flex items-center z-10 -ml-16 lg:-ml-24"
+      className="relative flex items-center group ml-4 md:ml-6"
     >
       {/* LOGO CONTAINER */}
       <div
@@ -14,16 +19,19 @@ const Logo = ({ scrolled }) => {
           transition-all duration-500 ease-in-out
           ${
             scrolled
-              ? "w-64 h-16"
-              : "w-[28rem] h-32 lg:w-[34rem] lg:h-36"
+              ? "w-32 h-10 md:w-36 md:h-12"
+              : "w-36 h-12 md:w-40 md:h-14"
           }
         `}
       >
         <img
           src={logo}
           alt="Tekacom logo"
-          className="w-full h-full object-contain scale-[1.18]"
+          className="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105"
         />
+        
+        {/* Effet de glow au hover */}
+        <div className="absolute inset-0 bg-gradient-to-r from-[#a34ee5] to-[#fec603] opacity-0 group-hover:opacity-20 blur-xl transition-all duration-500"></div>
       </div>
     </Link>
   );
