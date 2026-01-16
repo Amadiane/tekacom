7a421917eb40f95d910e3616000a565d2ccc5905
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-01-16 12:17:29 +0100

    contact ok fianl mail fonctionne confirme ousmane last

--- Client/src/components/Footer/Contacternous.jsx
+++ Client/src/components/Footer/Contacternous.jsx
@@ -436,9 +436,9 @@ const Contacternous = () => {
       </section>
 
       {/* ChatBot */}
-      <div className="fixed bottom-6 right-6 z-50">
+      {/* <div className="fixed bottom-6 right-6 z-50">
         <ChatBotNew />
-      </div>
+      </div> */}
 
       {/* Animations CSS */}
       <style>{`
